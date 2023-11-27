b7052b457c37edefccc9d6d57a895b9498838e5b
Raul Abril <raul.av91@gmail.com>
2023-11-27 11:11:09 +0000

    User logic

--- crm-react/src/main.tsx
+++ crm-react/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,7 +5,7 @@ import { store } from "./store/index.ts";
 
 import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>
